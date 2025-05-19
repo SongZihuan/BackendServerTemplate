db053a441e4039eb1ee2876f8288aead27618553
SongZihuan <songzihuan@song-zh.com>
2025-05-20 03:01:02 +0800

    修复 `go generate ./...` 调用错误并移除对 global 包的依赖

--- tool/generate/touch/main.go
+++ tool/generate/touch/main.go
@@ -5,7 +5,6 @@
 package main
 
 import (
-	"github.com/SongZihuan/BackendServerTemplate/global"
 	"github.com/SongZihuan/BackendServerTemplate/tool/generate/internal/exitreturn"
 	"github.com/SongZihuan/BackendServerTemplate/tool/generate/internal/touch"
 	"os"
@@ -17,12 +16,7 @@ func main() {
 
 // command 单独把 touch 作为一个程序，避免程序本身对 resource 包的依赖。
 func command() (exitcode int) {
-	err := global.GenerateTouchInit()
-	if err != nil {
-		return exitreturn.ReturnError(err)
-	}
-
-	err = touch.TouchBaseFile()
+	err := touch.TouchBaseFile()
 	if err != nil {
 		return exitreturn.ReturnError(err)
 	}
